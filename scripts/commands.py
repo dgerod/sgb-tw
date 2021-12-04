1abb58ea2f9e584030a7a91b7c367dd34fe577cc
Diego Escudero <dgerod@xyz-lab.org.es>
2021-12-04 07:53:29 +0100

    Remove sgb-tw and stored tiddlers this repository.
    Now this information is stored in an indpenedent directory that has ".sadac-tw" subdirectory. The sgb-cli tool searches for this subdirectory to know if it is a SGB documentation one, and user settings are stored in it.

--- scripts/commands.py
+++ scripts/commands.py
@@ -2,47 +2,45 @@
 
 import os
 import typing
-from string import Template   
-import yaml
 import shutil
-from bs4 import BeautifulSoup
 
 from scripts import db
 from scripts import tiddlers as td
+from scripts.configuration import Configuration
 
 
 def add_tiddlers():
-
-    CURRENT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
-       
-    ROOT_DIRECTORY = os.path.dirname(CURRENT_DIRECTORY)    
-    TEMP_DIRECTORY =  os.path.join(ROOT_DIRECTORY, 'tmp') 
-    TW_DIRECTORY = os.path.join(ROOT_DIRECTORY, 'db', 'tw')
     
-    tiddlers_directory = os.path.join(ROOT_DIRECTORY, 'db', 'tiddlers')
-    tiddlers = db.load_tiddlers(tiddlers_directory)
+    configuration = Configuration()
     
-    base_html_file = os.path.join(TW_DIRECTORY, 'sgb.html.base')    
-    new_html_file = os.path.join(TEMP_DIRECTORY, 'new_sgb.html')    
+    tiddlers_directory = os.path.join(configuration.source_directory(), 'tiddlers')
+    tw_directory = os.path.join(configuration.source_directory(), 'tw')        
+    tw_name = configuration.tw_name() + '.html'
+    tw_base_name = tw_name + '.base'
+    new_tw_name = 'new_' + tw_name
+    
+    base_html_file = os.path.join(tw_directory, tw_base_name) 
+    new_html_file = os.path.join(configuration.temp_directory(), new_tw_name)    
+    
+    tiddlers = db.load_tiddlers(tiddlers_directory)    
     td.upload_tiddlers(base_html_file, tiddlers, new_html_file)
         
-    tw_html_file = os.path.join(TW_DIRECTORY, 'sgb.html')    
+    tw_html_file = os.path.join(tw_directory, tw_name)    
     shutil.copyfile(new_html_file, tw_html_file)
     
     
 def retrieve_tiddlers():
-
-    CURRENT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
     
-    ROOT_DIRECTORY = os.path.dirname(CURRENT_DIRECTORY)
-    TEMP_DIRECTORY =  os.path.join(ROOT_DIRECTORY, 'tmp')    
-    TW_DIRECTORY = os.path.join(ROOT_DIRECTORY, 'db', 'tw')
-        
-    tw_html_file = os.path.join(TW_DIRECTORY, 'sgb.html')    
-    copied_html_file = os.path.join(TEMP_DIRECTORY, 'source_sgb.html')        
-    shutil.copyfile(tw_html_file, copied_html_file)
-          
-    tiddlers = td.extract_tiddlers(copied_html_file)    
-    tiddlers_directory = os.path.join(ROOT_DIRECTORY, 'db', 'tiddlers')
+    configuration = Configuration()
+    
+    tiddlers_directory = os.path.join(configuration.source_directory(), 'tiddlers')
+    tw_directory = os.path.join(configuration.source_directory(), 'tw')        
+    tw_name = configuration.tw_name() + '.html'
+    source_tw_name = 'source_' + tw_name 
+    
+    tw_html_file = os.path.join(tw_directory, tw_name)    
+    copied_html_file = os.path.join(configuration.temp_directory(), source_tw_name)        
+    
+    shutil.copyfile(tw_html_file, copied_html_file)          
+    tiddlers = td.extract_tiddlers(copied_html_file)        
     db.save_tiddlers(tiddlers, tiddlers_directory)
-
--- scripts/configuration.py
+++ scripts/configuration.py
@@ -1,12 +1,70 @@
 import os
+from pathlib import Path
+import yaml
+
 
 class Configuration:
 
-    def __init__(self):    
+    _ROOT_DIRECTORY = '.sadac-tw'    
+    _TEMP_DIRECTORY = _ROOT_DIRECTORY + os.sep + '_tmp_'
+    _CONFIGURATION_FILE = _ROOT_DIRECTORY + os.sep + 'settings.yaml'
+    
+    def __init__(self):
+        self._work_directory = self._find_directory()
+        self._configuration_file = os.path.join(self._work_directory, Configuration._CONFIGURATION_FILE)
+        self._source_directory = self._work_directory
+        self._tmp_directory = os.path.join(self._work_directory,
+                                           Configuration._TEMP_DIRECTORY)
+
+    def work_directory(self):
+        return self._work_directory
+    
+    def temp_directory(self):
+        return self._tmp_directory
+    
+    def source_directory(self):
+        return self._source_directory
+    
+    def tw_name(self):
+            
+        if not os.path.exists(self._configuration_file):
+            raise NotADirectoryError('Configuration file does not exist')
+            
+        with open(self._configuration_file, 'r') as stream:
+            try:
+                data = yaml.safe_load(stream)
+                tw_name = data['tw_name']
+                #if not os.path.isabs(source_directory):
+                #    source_directory = os.path.abspath(os.path.join(self._work_directory, docs['source_directory']))
+                    
+            except yaml.YAMLError as ex:
+                raise
+    
+        return tw_name
+
+    @staticmethod
+    def _find_directory():
+        """
+        Search directory where SADAC-TW stores all the information (.sadac)
+        """
+    
+        directory = ""
+        current_directory = os.getcwd()
         
-        CURRENT_DIRECTORY = os.path.dirname(os.path.abspath(__file__)) 
-        
-        self.root_directory = os.path.dirname(CURRENT_DIRECTORY)              
-        self.tmp_directory = os.path.join(self.root_directory, 'tmp') 
-        self.tiddlers_directory = os.path.join(self.root_directory, 'db', 'tiddlers')
-        self.tw_directory = os.path.join(self.root_directory, 'db', 'tw')
+        found = False
+        checking = True
+        while checking and not found:            
+    
+            found = Path(os.path.join(current_directory, Configuration._ROOT_DIRECTORY)).is_dir()
+            
+            if found:                
+                directory = current_directory                                
+            else:
+                previous_directory = current_directory            
+                current_directory = os.path.dirname(current_directory)
+                checking = current_directory != previous_directory
+                        
+        if not found:
+            raise NotADirectoryError('Directory not ready to work with SADAC-TW.')
+    
+        return directory
