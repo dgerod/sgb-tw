118006435ec22272b29a215f10cef97c01a19ee4
Diego Escudero <dgerod@xyz-lab.org.es>
2021-12-04 10:56:53 +0100

    Fix problem when a note has not defined tag, we store the empty list of tags in the DB.

--- scripts/tiddlers.py
+++ scripts/tiddlers.py
@@ -29,6 +29,9 @@ def extract_tiddlers(tw_file_path: str) -> typing.List:
     
     for element in our_tiddlers:
         
+        if not 'tags' in element:
+            element['tags'] = ''
+
         tiddler = {'title': element['title'],
                    'created': element['created'],
                    'creator': element['creator'],
@@ -38,16 +41,6 @@ def extract_tiddlers(tw_file_path: str) -> typing.List:
                    'tags': element['tags'],
                    'text': element['text']                  
                    }
-        
-        #print('TIDDLE ---')
-        #print('Title:', tiddler['title'])
-        #print('Type:', element['note_type'])
-        #print('Tags:', tiddler['tags'])
-        #print('Created:', tiddler['created'])
-        #print('Creator:', tiddler['creator'])
-        #print('Modified:', tiddler['modified'])
-        #print('Modifier:', tiddler['modifier'])
-        #print('Text:', tiddler['text'])
         
         if element['note_type'] == 'information':
             information_tiddlers.append(tiddler)
@@ -68,12 +61,9 @@ def upload_tiddlers(tw_file_path: str, tiddlers: typing.List, output_file_path: 
     html_data = open(tw_file_path, encoding='UTF-8').read()
     soup = BeautifulSoup(html_data, features="html.parser")
     element = soup.find('script',class_="tiddlywiki-tiddler-store", type="application/json")
-    #tiddlers_in_page = json.loads(element.string) 
-
+    
     new_tiddlers = []
     for tiddler in tiddlers:
-        
-        {"created":"20211119055559095","creator":"me","text":"","title":"Scope","note_type":"help","tags":"","modified":"20211119060805814","modifier":"me"},
         
         new_tiddler = { 'created': tiddler['meta']['created'],
                         'creator': tiddler['meta']['creator'],
@@ -82,7 +72,7 @@ def upload_tiddlers(tw_file_path: str, tiddlers: typing.List, output_file_path: 
                         'note_type': tiddler['meta']['note_type'],
                         'tags': tiddler['meta']['tags'],
                         'modified': tiddler['meta']['modified'],
-                        'modifier': tiddler['meta']['modifier'] }
+                        'modifier': tiddler['meta']['modifier'] }        
         
         new_tiddlers.append(new_tiddler)
 
